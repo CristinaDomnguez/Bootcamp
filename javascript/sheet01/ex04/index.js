f3e8b8bdf1ee0d783c64567297e0406876f40833
Cristina Domínguez <dominguezlanzacristina@gmail.com>
2024-11-12 12:28:24 +0100

    feat:Condicionales sin funcion ex04

---
+++ javascript/sheet01/ex04/index.js
@@ -0,0 +1,24 @@
+let a = -43;
+if (a < 0) {
+  console.log("El numero " + a + " es negativo");
+} else if (a == 0) {
+  console.log("El numero " + a + " es cero");
+} else {
+  console.log("El numero " + a + " es positivo");
+}
+let b = 0;
+if (b < 0) {
+  console.log("El numero " + b + " es negativo");
+} else if (b == 0) {
+  console.log("El numero " + b + " es cero");
+} else {
+  console.log("El numero " + b + " es positivo");
+}
+let c = 13;
+if (c < 0) {
+  console.log("El numero " + c + " es negativo");
+} else if (c == 0) {
+  console.log("El numero " + c + " es cero");
+} else {
+  console.log("El numero " + c + " es positivo");
+}
