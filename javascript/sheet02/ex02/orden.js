f08488b1511920a5ed272c7ff27b0ef122524df7
Cristina Domínguez <dominguezlanzacristina@gmail.com>
2024-11-19 13:52:46 +0100

    feat:orden palabras ex02

---
+++ javascript/sheet02/ex02/orden.js
@@ -0,0 +1,6 @@
+const palabras = ['hola', 'adios', 'paciencia', 'ayuda', 'amor'];
+function ordenarPalabras(palabras){
+    const ordenado = palabras.sort();
+    return ordenado;
+}
+console.log(ordenarPalabras(palabras));
