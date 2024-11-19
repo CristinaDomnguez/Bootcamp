ee3553a55c1e689910d9629c7cf9c46c7aab8aee
Cristina Domínguez <dominguezlanzacristina@gmail.com>
2024-11-19 13:54:40 +0100

    fix:error modf ex01 ex02

--- javascript/sheet02/ex01/impares.js
+++ javascript/sheet02/ex01/impares.js
@@ -1,5 +1,5 @@
 const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-function filtrarImpares(numeros){
+function filtrarImpares([]){
     const impares = numeros.filter(num => num % 2 !== 0);
     return impares;
 }
