3bf5f27b60b63cce22803556d6f38e6ff3940733
Cristina Domínguez <dominguezlanzacristina@gmail.com>
2024-11-28 13:22:21 +0100

    feat: variables y tipo de datos java

---
+++ Java/sheet01/ex02/src/VariablesTipoDatos.java
@@ -0,0 +1,15 @@
+public class VariablesTipoDatos {
+    public static void main(String[] args){
+    int numeroEntero = 3;
+    double numeroDecimal = 3.33;
+    boolean boleano = true;
+    char caracter = 'a';
+    String frase = "Esto es un frase";
+
+    System.out.println("Esto es un numero entero: " + numeroEntero);
+    System.out.println("Esto es un numero decimal: " + numeroDecimal);
+    System.out.println("Esto es un boleano: " + boleano);
+    System.out.println("Esto es un caracter: " + caracter);
+    System.out.println("Esto es una frase: " + frase);
+    }
+}
