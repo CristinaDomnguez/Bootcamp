8a399f2a977c9eb48ecdad80ae55846f0f21268a
Cristina Domínguez <dominguezlanzacristina@gmail.com>
2024-11-29 18:12:11 +0100

    feat:funciones

---
+++ Java/sheet01/ex07/src/Funciones.java
@@ -0,0 +1,10 @@
+public class Funciones {
+    public static void main(String[] args){
+        int resultado = suma(3, 2);
+        System.out.println("El resultado de la suma es: " + resultado);
+        }
+
+    public static int suma(int a, int b) {
+        return a + b;
+    }
+}
