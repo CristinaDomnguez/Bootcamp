e6721c25cd5ca8813cfaf0174143a0170d7365df
Cristina Domínguez <dominguezlanzacristina@gmail.com>
2024-12-04 10:37:43 +0100

    feat:ex05 hashSet

---
+++ Java/sheet03/ExercisesSheet03/src/HashSetExample.java
@@ -0,0 +1,14 @@
+import java.util.HashSet;
+
+public class HashSetExample {
+    public static void main(String[] args){
+        HashSet<Integer> numeros = new HashSet<>();
+            numeros.add(10);
+            numeros.add(20);
+            numeros.add(30);
+
+        for (int numero : numeros) {
+            System.out.println(numero);
+        }
+    }
+}
